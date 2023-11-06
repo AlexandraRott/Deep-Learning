4b61bdb0523541fb0afd90d96b7e3b6fb2b4f28c
AlexandraRott <sasha_germany@mail.ru>
2023-11-06 04:45:23 +0100

    script update

--- server/app.js
+++ server/app.js
@@ -9,34 +9,31 @@ const port = 3000;
 const storage = multer.memoryStorage(); // Меняем на memoryStorage для обработки в памяти
 const upload = multer({ storage: storage });
 
-app.use(express.static(path.join(__dirname, '../images'))); // Позволяет обслуживать статические файлы
+app.use(express.static(path.join(__dirname, '../images')));
 
 app.get('/', (req, res) => {
+    console.log('Received a request for the homepage'); // Логирование информационного сообщения
     res.sendFile(path.join(__dirname, '../index.html'));
 });
-
-// Разрешаем доступ к статическим файлам (изображениям)
-app.use(express.static(path.join(__dirname, '../')));
 
 app.post('/upload', upload.single('file'), async (req, res) => {
     try {
         const buffer = req.file.buffer;
         const resizedImage = await sharp(buffer)
-            .resize({ width: 500, height: 500, fit: 'inside' }) // Масштабируем изображение до 500px с сохранением пропорций
-            .toBuffer(); // Преобразуем в буфер
+            .resize({ width: 500, height: 500, fit: 'inside' })
+            .toBuffer();
 
-        // Загруженное изображение будет сохранено с новым размером
         const imageName = `upload/${req.file.originalname}`;
         await sharp(resizedImage).toFile(path.join(__dirname, '../', imageName));
 
-        // Возвращаем URL загруженного изображения
         res.send(imageName);
     } catch (error) {
-        console.error('Image processing error:', error);
+        console.error('Image processing error:', error); // Логирование ошибки
         res.status(500).send('Image processing error');
     }
 });
 
 app.listen(port, () => {
-    console.log(`Server runs on port: ${port}`);
+    console.log(`Server runs on port: ${port}`); // Логирование информационного сообщения
 });
+
