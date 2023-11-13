09c0ef4753999547dac43a663335acac889586ff
AlexandraRott <sasha_germany@mail.ru>
2023-11-13 16:49:09 +0100

    Regression tab added

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,13 +3,43 @@ const uploadForm = document.getElementById('uploadForm');
 const uploadedImage = document.getElementById('uploadedImage');
 const imagePreview = document.getElementById('imagePreview');
 const documentationContent = document.getElementById('documentationContent');
-const examplesContent = document.getElementById('examplesContent'); // Добавлен элемент для отображения примеров
-
+const examplesContent = document.getElementById('examplesContent');
+const regressionContent = document.getElementById('regressionContent');
 const analyzeButton = document.getElementById('analyzeButton');
+const regressionChartCanvas = document.getElementById('regressionChart');
 
-analyzeButton.addEventListener('click', () => {
-    // Анализируем изображение
-    analyzeImage(imagePreview.src);
+// Вызываем функцию для отображения примеров изображений при загрузке страницы
+displayExampleImages();
+
+const contentBlocks = {
+    upload: () => {
+        uploadForm.style.display = 'block';
+        uploadedImage.style.display = 'none';
+        documentationContent.style.display = 'none';
+        examplesContent.style.display = 'none';
+        regressionContent.style.display = 'none';
+
+    },
+    documentation: () => {
+        uploadForm.style.display = 'none';
+        uploadedImage.style.display = 'none';
+        documentationContent.style.display = 'block';
+        examplesContent.style.display = 'none';
+        regressionContent.style.display = 'none';
+    },
+    examples: () => {
+        displayExampleImages(); // Вызываем функцию для отображения примеров
+    }
+};
+
+buttons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        event.preventDefault();
+        const id = button.getAttribute('id');
+        if (contentBlocks[id]) {
+            contentBlocks[id]();
+        }
+    });
 });
 
 // Функция для изменения размера изображения
@@ -93,40 +123,14 @@ function displayExampleImages() {
     uploadForm.style.display = 'none';
     uploadedImage.style.display = 'none';
     documentationContent.style.display = 'none';
+    regressionContent.style.display = 'none';
 }
 
 
 
-
-// Вызываем функцию для отображения примеров изображений при загрузке страницы
-displayExampleImages();
-
-const contentBlocks = {
-    upload: () => {
-        uploadForm.style.display = 'block';
-        uploadedImage.style.display = 'none';
-        documentationContent.style.display = 'none';
-        examplesContent.style.display = 'none';
-    },
-    documentation: () => {
-        uploadForm.style.display = 'none';
-        uploadedImage.style.display = 'none';
-        documentationContent.style.display = 'block';
-        examplesContent.style.display = 'none';
-    },
-    examples: () => {
-        displayExampleImages(); // Вызываем функцию для отображения примеров
-    }
-};
-
-buttons.forEach(button => {
-    button.addEventListener('click', (event) => {
-        event.preventDefault();
-        const id = button.getAttribute('id');
-        if (contentBlocks[id]) {
-            contentBlocks[id]();
-        }
-    });
+analyzeButton.addEventListener('click', () => {
+    // Анализируем изображение
+    analyzeImage(imagePreview.src);
 });
 
 const uploadImageForm = document.getElementById('uploadImageForm');
@@ -156,7 +160,6 @@ uploadImageForm.addEventListener('submit', async (event) => {
 
 
 // Функция анализа изображения с использованием ml5.js
-// Функция для анализа изображения с использованием ml5.js
 function analyzeImage(imageUrl, container, resultId) {
     const classifier = ml5.imageClassifier('MobileNet', () => {
         const img = new Image();
@@ -220,4 +223,116 @@ fileInput.addEventListener('change', async (event) => {
         console.error('Upload error:', error);
     }
 });
+
+
+function initDefaultRegressionChart() {
+    regressionContent.style.display = 'block';
+
+    const regressionChartElement = document.getElementById('regressionChart');
+
+    const options = {
+        chart: {
+            type: 'line',
+            height: 500,
+        },
+        series: [
+            {
+                name: 'y = a*x + b',
+                data: [],
+            },
+        ],
+        xaxis: {
+            categories: [],
+        },
+    };
+
+    const regressionChart = new ApexCharts(regressionChartElement, options);
+    regressionChart.render();
+
+    // Сохраните объект графика в глобальной переменной
+    window.regressionChart = regressionChart;
+
+    // Установите значения по умолчанию (a=1, b=0)
+    updateRegressionChart(1, 0);
+}
+
+contentBlocks.regression = () => {
+    initDefaultRegressionChart();
+    uploadForm.style.display = 'none';
+    uploadedImage.style.display = 'none';
+    documentationContent.style.display = 'none';
+    examplesContent.style.display = 'none';
+};
+
+
+const updateRegressionButton = document.getElementById('updateRegression');
+updateRegressionButton.addEventListener('click', () => {
+    // Получите значения a и b из инпутов
+    const a = parseFloat(document.getElementById('inputA').value);
+    const b = parseFloat(document.getElementById('inputB').value);
+
+    // Обновите график с новой функцией y = x * a + b
+    updateRegressionChart(a, b);
+});
+
+// Функция для обновления графика с новыми значениями a и b
+function updateRegressionChart(a, b) {
+    // Получите объект графика
+    const regressionChart = window.regressionChart;
+
+    // Обновите данные для графика на основе новых значений a и b
+    const newData = [];
+    for (let x = -10; x <= 10; x++) {
+        newData.push(x * a + b);
+    }
+
+    // Обновите данные графика
+    regressionChart.updateSeries([
+        {
+            name: 'y = ax+b',
+            data: newData,
+        },
+    ]);
+}
+
+// Функция для построения графика
+function createRegressionChart() {
+    regressionContent.style.display = 'block';
+
+    const regressionChartElement = document.getElementById('regressionChart');
+
+    const options = {
+        chart: {
+            type: 'line',
+            height: 500,
+        },
+        series: [
+            {
+                name: 'y = ax+b',
+                data: [],
+            },
+        ],
+        xaxis: {
+            categories: [],
+        },
+    };
+
+    const regressionChart = new ApexCharts(regressionChartElement, options);
+    regressionChart.render();
+
+    // Сохраните объект графика в глобальной переменной
+    window.regressionChart = regressionChart;
+
+    // Установите значения по умолчанию (a=1, b=0)
+    updateRegressionChart(1, 0);
+}
+
+// Добавьте функцию для отображения раздела "Regression" при выборе соответствующего пункта меню
+contentBlocks.regression = () => {
+    createRegressionChart();
+    uploadForm.style.display = 'none';
+    uploadedImage.style.display = 'none';
+    documentationContent.style.display = 'none';
+    examplesContent.style.display = 'none';
+};
 
--- server/app.js
+++ server/app.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
-const sharp = require('sharp'); // Подключаем библиотеку sharp
+const sharp = require('sharp');
 const app = express();
 
-// Установите желаемый порт
 const port = process.env.PORT || 3000;
 
-// Настройка папки для загрузки изображений
-const storage = multer.memoryStorage(); // Меняем на memoryStorage для обработки в памяти
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
-    console.log('Received a request for the homepage'); // Логирование информационного сообщения
+    console.log('Received a request for the homepage');
     res.sendFile(path.join(__dirname, '../index.html'));
 });
 
@@ -22,19 +20,20 @@ app.post('/upload', upload.single('file'), async (req, res) => {
     try {
         const buffer = req.file.buffer;
         const resizedImage = await sharp(buffer)
-            .resize({ width: 500, height: 500, fit: 'inside' })
+            .resize({ width: 300, height: 300, fit: 'inside' })
             .toBuffer();
 
         const imageName = `upload/${req.file.originalname}`;
         await sharp(resizedImage).toFile(path.join(__dirname, '../public', imageName));
 
+        console.log(`File "${req.file.originalname}" uploaded successfully.`); // Логирование успешной загрузки
         res.send(imageName);
     } catch (error) {
-        console.error('Image processing error:', error); // Логирование ошибки
+        console.error('Image processing error:', error);
         res.status(500).send('Image processing error');
     }
 });
 
 app.listen(port, () => {
-    console.log(`Server runs on port: ${port}`); // Логирование информационного сообщения
+    console.log(`Server runs on port: ${port}`);
 });
